dba493406dfe8c96aa1e1154e0c0a28f7227528e
Nickky484 <161592287+Nickky484@users.noreply.github.com>
2024-05-22 01:22:35 +0700

    Add files via upload

---
+++ DemoDocghi.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.demodocghi;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class DemoDocghi {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
